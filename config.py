0187799b53cb4c0c639e1168a40a0cabe14f1750
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-02-18 23:45:53 +0200

    v1.0

--- config.py
+++ config.py
@@ -1,20 +1,25 @@
-from os import getcwd
+""" Google BigQuery to CSV downloader config file. """
 
-
-# credentials
-KEY_path = '{}/{}'.format(getcwd(), 'key.json')
+'''
+Credentials file path.
+Write here absolute path to JSON key file to your Google BigQuery accaunt
+The key may be generated here: 
+https://console.cloud.google.com/apis/credentials/serviceaccountkey
+Example: '/home/data/key.json'
+'''
+KEY_PATH = 'place_path_here'
 
 # SQL query to execute
-SQL_QUERY = 'SELECT * FROM temp.stories_copy LIMIT 10 offset 30'
+SQL_QUERY = 'place_sql_here'
 
-# downloaded file options
-FILE_PATH = '{}'.format(getcwd())
-FILE_NAME = 'data.csv'
-FILE_SIZE = 5  # int, KByte
-FILE_COUNT = 3
+# Data file options
+FILE_PATH = ''  # Absolute path to data files folder. Ex: '/home/data/'
+FILE_NAME = ''  # Data file name without extansion. Ex: 'data'
+FILE_SIZE = 0  # Data file size in KBytes
+FILE_COUNT = 0  # Number of data files to store
 
-# error_log file options
-ERROR_FILE_PATH = ''
-ERROR_FILE_NAME = ''
-ERROR_FILE_SIZE = ''
-ERROR_FILE_COUNT = ''
+# Error file options
+ERROR_FILE_PATH = ''  # Absolute path to error files folder. Ex: '/home/data/errors/'
+ERROR_FILE_NAME = ''  # Error file name without extansion. Ex: 'error'
+ERROR_FILE_SIZE = 0  # Error file size in KBytes
+ERROR_FILE_COUNT = 0  # Number of error files to store
--- gbq_script.py
+++ gbq_script.py
@@ -7,49 +7,133 @@ from google.cloud import bigquery
 
 import config
 
-
-# encoding=utf8
-reload(sys)
-sys.setdefaultencoding('utf8')
+DATA_FP = '{}/{}.csv'.format(config.FILE_PATH, config.FILE_NAME)
+ERROR_FP = '{}/{}.log'.format(config.ERROR_FILE_PATH, config.ERROR_FILE_NAME)
 
 
-def rollover_logfile():
-    # rename current file
-    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    new_filename = '{}-{}'.format(config.FILE_NAME, now)
-    os.rename(config.FILE_NAME, new_filename)
-    # todo write file extansion after date
+def init():
+    """
+    Make some preparations before downloading process
+    """
+    # register credentials
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.KEY_PATH
 
-    # check files count and delete old files if need
-    # filter log files from all files in directory
-    files = filter(lambda x: True if config.FILE_NAME in x else False,
-                   os.listdir(config.FILE_PATH))
-    files = sorted(files, key=os.path.getctime)
+    # add utf-8 support (python 2.7 crutch)
+    reload(sys)
+    sys.setdefaultencoding('utf8')
 
-    if len(files) >= config.FILE_COUNT:
-        os.remove('{}/{}'.format(config.FILE_PATH, files[0]))
+    # create data- and log-files (like touch)
+    with open(DATA_FP, 'a') as f:
+        pass
+    with open(ERROR_FP, 'a') as f:
+        pass
 
 
-with open(config.FILE_NAME, 'a') as f:
-    pass
+def get_now():
+    return datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.KEY_path
 
-client = bigquery.Client()
-query_job = client.query(query=config.SQL_QUERY)
-
-for row in query_job:
-    # data = [i.encode('utf-8') if isinstance(i, str) else i for i in row.values()]
-    data = row.values()
-    # get curent file size
-    file_size = os.path.getsize('{}/{}'.format(config.FILE_PATH, config.FILE_NAME))
+def check_file_size(data, error=False):
+    """
+    Checks if file already too large for adding extra information
+    In that case it calls `rollover` func
+    :param data: data that will be written to file
+    :param error: bool. True if data is error message, False if it data row
+    """
+    current_file_size = os.path.getsize(DATA_FP if not error else ERROR_FP)
+    max_file_size = config.FILE_SIZE if not error else config.ERROR_FILE_SIZE
+    max_file_size *= 1024  # because there are KBytes in config
     data_size = sys.getsizeof(data)
-    max_file_size = config.FILE_SIZE * 1024
 
-    # file too big?
-    if file_size + data_size > max_file_size:
-        rollover_logfile()
+    if current_file_size + data_size > max_file_size:
+        rollover(error=error)
 
-    with open(config.FILE_NAME, 'ab', ) as logfile:
-        log_writer = writer(logfile, delimiter=',', quotechar='"')
-        log_writer.writerow(data)
+
+def write_error_to_log(msg):
+    """
+    Writes error message to error file.
+    Message format:
+        yyyy-mm-dd_hh-mm-ss, error type
+        Error message
+        ========================
+    :param msg: Error type
+    """
+    check_file_size(msg, error=True)
+
+    with open(ERROR_FP, 'ab') as error_log:
+        error_log.write(msg)
+
+
+def rollover(error=False):
+    """
+    Renames current file to 'archive' version with datetime in filename,
+    and delete too old files if count of them more then value in config
+    :param error: bool. True if error file, False if  data file
+    :return:
+    """
+    file_name = config.FILE_NAME if not error else config.ERROR_FILE_NAME
+    file_path = config.FILE_PATH if not error else config.ERROR_FILE_PATH
+    file_count = config.FILE_COUNT if not error else config.ERROR_FILE_COUNT
+    extension = 'csv' if not error else 'log'
+
+    # rename current file
+    new_filename = '{}-{}.{}'.format(file_name, get_now(), extension)
+    os.rename('{}.{}'.format(file_name, extension), new_filename)
+
+    # Check files count and delete old files if need
+
+    # filter data or error files from all files in directory
+    files = filter(lambda x: True if file_name in x else False, os.listdir(file_path))
+    # sorting files by creation date
+    files = sorted(files, key=os.path.getctime)
+    if len(files) >= file_count:
+        # deleting the oldest
+        os.remove('{}/{}'.format(file_path, files[0]))
+
+
+def load_data():
+    """
+    Main function. Connects to Google BigQuery, executes query, loads data
+    and writes it to csv file
+    """
+
+    # Check connection
+    try:
+        client = bigquery.Client()
+    except Exception as e:
+        error_message = '{}, {}\n{}\n========================\n'.format(
+            get_now(), 'Connection error', e)
+        write_error_to_log(error_message)
+        exit()
+    else:
+        query_job = client.query(query=config.SQL_QUERY)
+
+    # Check if query is executable
+    try:
+        for row in query_job:
+            pass
+    except Exception as e:
+        error_message = '{}, {}\n{}========================\n'.format(
+            get_now(), 'Query executing error', e)
+        write_error_to_log(error_message)
+        exit()
+
+    # Working with data row-per-row
+    for row in query_job:
+        data = row.values()
+        check_file_size(data)
+
+        # Writing row to csv
+        with open(DATA_FP, 'ab', ) as logfile:
+            log_writer = writer(logfile, delimiter=',', quotechar='"')
+            try:
+                log_writer.writerow(data)
+            except Exception as e:
+                error_message = '{}, {}\n{}========================\n'.format(
+                    get_now(), 'Writing to file error', e)
+                write_error_to_log(error_message)
+
+
+if __name__ == '__main__':
+    init()
+    load_data()
