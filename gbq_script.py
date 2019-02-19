74fb01231dd49a2268acdbe6eff783f1da506228
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-02-19 14:27:28 +0200

    Bug fixes

--- gbq_script.py
+++ gbq_script.py
@@ -10,6 +10,12 @@ import config
 DATA_FP = '{}/{}.csv'.format(config.FILE_PATH, config.FILE_NAME)
 ERROR_FP = '{}/{}.log'.format(config.ERROR_FILE_PATH, config.ERROR_FILE_NAME)
 
+FILE_HANDLERS = {
+    'data': open(DATA_FP, 'ab'),
+    'error': open(ERROR_FP, 'ab')
+}
+DATA_WRITER = {'dw': writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')}
+
 
 def init():
     """
@@ -22,46 +28,36 @@ def init():
     reload(sys)
     sys.setdefaultencoding('utf8')
 
-    # create data- and log-files (like touch)
-    with open(DATA_FP, 'a') as f:
-        pass
-    with open(ERROR_FP, 'a') as f:
-        pass
-
 
 def get_now():
     return datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 
-def check_file_size(data, error=False):
+def check_file_size(data, max_file_size, error=False):
     """
     Checks if file already too large for adding extra information
     In that case it calls `rollover` func
+    :param max_file_size:
     :param data: data that will be written to file
     :param error: bool. True if data is error message, False if it data row
     """
     current_file_size = os.path.getsize(DATA_FP if not error else ERROR_FP)
-    max_file_size = config.FILE_SIZE if not error else config.ERROR_FILE_SIZE
-    max_file_size *= 1024  # because there are KBytes in config
     data_size = sys.getsizeof(data)
 
     if current_file_size + data_size > max_file_size:
         rollover(error=error)
 
 
-def write_error_to_log(msg):
+def write_error_to_log(exception, description):
     """
     Writes error message to error file.
-    Message format:
-        yyyy-mm-dd_hh-mm-ss, error type
-        Error message
-        ========================
-    :param msg: Error type
+    :param exception: exception data
+    :param description: exception description
     """
-    check_file_size(msg, error=True)
-
-    with open(ERROR_FP, 'ab') as error_log:
-        error_log.write(msg)
+    msg = '{}, {}\n{}\n========================\n'.format(
+        get_now(), description, exception)
+    check_file_size(msg, config.ERROR_FILE_SIZE * 1024, error=True)
+    FILE_HANDLERS['error'].write(msg)
 
 
 def rollover(error=False):
@@ -71,10 +67,14 @@ def rollover(error=False):
     :param error: bool. True if error file, False if  data file
     :return:
     """
+    file_type = 'data' if not error else 'error'
     file_name = config.FILE_NAME if not error else config.ERROR_FILE_NAME
     file_path = config.FILE_PATH if not error else config.ERROR_FILE_PATH
     file_count = config.FILE_COUNT if not error else config.ERROR_FILE_COUNT
     extension = 'csv' if not error else 'log'
+
+    # close file before editing it
+    FILE_HANDLERS[file_type].close()
 
     # rename current file
     new_filename = '{}-{}.{}'.format(file_name, get_now(), extension)
@@ -90,6 +90,12 @@ def rollover(error=False):
         # deleting the oldest
         os.remove('{}/{}'.format(file_path, files[0]))
 
+    # reopening file
+    FILE_HANDLERS[file_type] = open(DATA_FP if not error else ERROR_FP, 'ab')
+    # recreate csv writer
+    if not error:
+        DATA_WRITER['dw'] = writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')
+
 
 def load_data():
     """
@@ -101,39 +107,34 @@ def load_data():
     try:
         client = bigquery.Client()
     except Exception as e:
-        error_message = '{}, {}\n{}\n========================\n'.format(
-            get_now(), 'Connection error', e)
-        write_error_to_log(error_message)
-        exit()
+        write_error_to_log(e, 'Connection error')
+        return
     else:
         query_job = client.query(query=config.SQL_QUERY)
 
     # Check if query is executable
     try:
-        for row in query_job:
-            pass
+        result = query_job.result()
     except Exception as e:
-        error_message = '{}, {}\n{}========================\n'.format(
-            get_now(), 'Query executing error', e)
-        write_error_to_log(error_message)
-        exit()
+        write_error_to_log(e, 'Query executing error')
+        return
 
     # Working with data row-per-row
-    for row in query_job:
-        data = row.values()
-        check_file_size(data)
+    for row in result:
+        check_file_size(str(row), config.FILE_SIZE * 1024)
 
-        # Writing row to csv
-        with open(DATA_FP, 'ab', ) as logfile:
-            log_writer = writer(logfile, delimiter=',', quotechar='"')
-            try:
-                log_writer.writerow(data)
-            except Exception as e:
-                error_message = '{}, {}\n{}========================\n'.format(
-                    get_now(), 'Writing to file error', e)
-                write_error_to_log(error_message)
+        try:
+            DATA_WRITER['dw'].writerow(row.values())
+        except Exception as e:
+            write_error_to_log(e, 'Writing to file error')
+
+
+def on_finish():
+    for f in FILE_HANDLERS.values():
+        f.close()
 
 
 if __name__ == '__main__':
     init()
     load_data()
+    on_finish()
