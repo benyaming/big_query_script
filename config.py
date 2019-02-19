60faab63795c850c3693022a6226e960df2880ad
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-02-19 16:57:36 +0200

    Code improvements

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ KEY_PATH = 'place_path_here'
 SQL_QUERY = 'place_sql_here'
 
 # Data file options
-FILE_PATH = ''  # Absolute path to data files folder. Ex: '/home/data/'
+FILE_FILDER = ''  # Absolute path to data files folder. Ex: '/home/data/'
 FILE_NAME = ''  # Data file name without extansion. Ex: 'data'
 FILE_SIZE = 0  # Data file size in KBytes
 FILE_COUNT = 0  # Number of data files to store
 
 # Error file options
-ERROR_FILE_PATH = ''  # Absolute path to error files folder. Ex: '/home/data/errors/'
+ERROR_FILE_FOLDER = ''  # Absolute path to error files folder. Ex: '/home/data/errors/'
 ERROR_FILE_NAME = ''  # Error file name without extansion. Ex: 'error'
 ERROR_FILE_SIZE = 0  # Error file size in KBytes
 ERROR_FILE_COUNT = 0  # Number of error files to store
--- gbq_script.py
+++ gbq_script.py
@@ -7,22 +7,21 @@ from google.cloud import bigquery
 
 import config
 
-DATA_FP = '{}/{}.csv'.format(config.FILE_PATH, config.FILE_NAME)
-ERROR_FP = '{}/{}.log'.format(config.ERROR_FILE_PATH, config.ERROR_FILE_NAME)
 
 FILE_HANDLERS = {
-    'data': open(DATA_FP, 'ab'),
-    'error': open(ERROR_FP, 'ab')
+    'data': open('{}/{}.csv'.format(config.FILE_FILDER, config.FILE_NAME), 'ab'),
+    'error': open('{}/{}.log'.format(config.ERROR_FILE_FOLDER, config.ERROR_FILE_NAME),
+                  'ab')
 }
-DATA_WRITER = {'dw': writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')}
+FILE_HANDLERS['dw'] = writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')
 
 
-def init():
+def on_start():
     """
     Make some preparations before downloading process
     """
     # register credentials
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.KEY_PATH
+    # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.KEY_PATH
 
     # add utf-8 support (python 2.7 crutch)
     reload(sys)
@@ -33,48 +32,29 @@ def get_now():
     return datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 
-def check_file_size(data, max_file_size, error=False):
+def check_file_size(data, current_file_size, max_file_size):
     """
-    Checks if file already too large for adding extra information
-    In that case it calls `rollover` func
-    :param max_file_size:
-    :param data: data that will be written to file
-    :param error: bool. True if data is error message, False if it data row
+    Checks file size. Returns True if there is no more place
     """
-    current_file_size = os.path.getsize(DATA_FP if not error else ERROR_FP)
     data_size = sys.getsizeof(data)
 
     if current_file_size + data_size > max_file_size:
-        rollover(error=error)
+        return True
 
 
-def write_error_to_log(exception, description):
-    """
-    Writes error message to error file.
-    :param exception: exception data
-    :param description: exception description
-    """
-    msg = '{}, {}\n{}\n========================\n'.format(
-        get_now(), description, exception)
-    check_file_size(msg, config.ERROR_FILE_SIZE * 1024, error=True)
-    FILE_HANDLERS['error'].write(msg)
-
-
-def rollover(error=False):
+def rollover(f, file_count):
     """
     Renames current file to 'archive' version with datetime in filename,
-    and delete too old files if count of them more then value in config
-    :param error: bool. True if error file, False if  data file
+    and deletes too old files if count of them more then value in config
+    :param file_count:
+    :param f:
     :return:
     """
-    file_type = 'data' if not error else 'error'
-    file_name = config.FILE_NAME if not error else config.ERROR_FILE_NAME
-    file_path = config.FILE_PATH if not error else config.ERROR_FILE_PATH
-    file_count = config.FILE_COUNT if not error else config.ERROR_FILE_COUNT
-    extension = 'csv' if not error else 'log'
+    file_path = f.name
+    file_name, extension = file_path.split('/')[-1].split('.')
 
     # close file before editing it
-    FILE_HANDLERS[file_type].close()
+    f.close()
 
     # rename current file
     new_filename = '{}-{}.{}'.format(file_name, get_now(), extension)
@@ -83,18 +63,41 @@ def rollover(error=False):
     # Check files count and delete old files if need
 
     # filter data or error files from all files in directory
-    files = filter(lambda x: True if file_name in x else False, os.listdir(file_path))
+    folder_path = file_path.split(file_name)[0]
+    files = filter(lambda x: True if file_name in x else False, os.listdir(folder_path))
     # sorting files by creation date
     files = sorted(files, key=os.path.getctime)
     if len(files) >= file_count:
         # deleting the oldest
-        os.remove('{}/{}'.format(file_path, files[0]))
+        os.remove('{}/{}'.format(folder_path, files[0]))
 
-    # reopening file
-    FILE_HANDLERS[file_type] = open(DATA_FP if not error else ERROR_FP, 'ab')
-    # recreate csv writer
-    if not error:
-        DATA_WRITER['dw'] = writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')
+    return open(file_path, 'ab')
+
+
+def write_error(exception, description):
+    """
+    Writes error message to error file.
+    :param exception: exception data
+    :param description: exception description
+    """
+    msg = '{}, {}\n{}\n========================\n'.format(
+        get_now(), description, exception)
+    current_file_size = os.path.getsize(FILE_HANDLERS['error'].name)
+    file_is_full = check_file_size(msg, current_file_size, config.ERROR_FILE_SIZE * 1024)
+    if file_is_full:
+        fc = config.ERROR_FILE_COUNT
+        FILE_HANDLERS['error'] = rollover(FILE_HANDLERS['error'], fc)
+    FILE_HANDLERS['error'].write(msg)
+
+
+def write_data(data):
+    current_file_size = os.path.getsize(FILE_HANDLERS['data'].name)
+    file_is_full = check_file_size(data, current_file_size, config.FILE_SIZE * 1024)
+    if file_is_full:
+        fc = config.FILE_COUNT
+        FILE_HANDLERS['data'] = rollover(FILE_HANDLERS['data'], fc)
+        FILE_HANDLERS['dw'] = writer(FILE_HANDLERS['data'], delimiter=',', quotechar='"')
+    FILE_HANDLERS['dw'].writerow(data)
 
 
 def load_data():
@@ -107,7 +110,7 @@ def load_data():
     try:
         client = bigquery.Client()
     except Exception as e:
-        write_error_to_log(e, 'Connection error')
+        write_error(e, 'Connection error')
         return
     else:
         query_job = client.query(query=config.SQL_QUERY)
@@ -116,25 +119,23 @@ def load_data():
     try:
         result = query_job.result()
     except Exception as e:
-        write_error_to_log(e, 'Query executing error')
+        write_error(e, 'Query executing error')
         return
 
     # Working with data row-per-row
     for row in result:
-        check_file_size(str(row), config.FILE_SIZE * 1024)
-
         try:
-            DATA_WRITER['dw'].writerow(row.values())
+            write_data(row.values())
         except Exception as e:
-            write_error_to_log(e, 'Writing to file error')
+            write_error(e, 'Writing to file error')
 
 
 def on_finish():
-    for f in FILE_HANDLERS.values():
-        f.close()
+    FILE_HANDLERS['error'].close()
+    FILE_HANDLERS['data'].close()
 
 
 if __name__ == '__main__':
-    init()
+    on_start()
     load_data()
     on_finish()
